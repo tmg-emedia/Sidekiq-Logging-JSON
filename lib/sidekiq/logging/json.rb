67f9b6a1750d441e1c1adc610332c91c5158c00e
Pedro Garrett <pedro.garrett@emediaweb.com>
2017-08-03 12:19:56 -0400

    attempted logstsh fix

--- lib/sidekiq/logging/json.rb
+++ lib/sidekiq/logging/json.rb
@@ -17,7 +17,7 @@ module Sidekiq
             jid = nil
             worker = nil
           end
-          {
+          result = {
             '@timestamp' => time.utc.iso8601,
             '@fields' => {
               :pid => ::Process.pid,
@@ -30,23 +30,33 @@ module Sidekiq
             '@status' => nil,
             '@severity' => severity,
             '@run_time' => nil,
-          }.merge(process_message(message)).to_json + "\n"
+          }
+              
+          pm = process_message message
+          if pm['@message'].is_a? String
+            result = result.merge(pm)
+          else
+            result['@fields'][:msg] = pm['@message']
+            result['@message'] = 'see fields'
+          end
+
+          result.to_json + "\n"
         end
 
         def process_message(message)
           case message
           when Exception
-            { '@status' => 'exception', '@message' => { 'msg' => message.message } }
+            { '@status' => 'exception', '@message' => message.message }
           when Hash
             if message["retry"]
               {
                 '@status' => 'retry',
-                '@message' => { 'msg' => "#{message['class']} failed, retrying with args #{message['args']}." }
+                '@message' => "#{message['class']} failed, retrying with args #{message['args']}."
               }
             elsif message['class'] && message['args']
               {
                 '@status' => 'dead',
-                '@message' => { 'msg' => "#{message['class']} failed with args #{message['args']}, not retrying." }
+                '@message' => "#{message['class']} failed with args #{message['args']}, not retrying."
               }
             else
               { '@message' => message }
@@ -58,7 +68,7 @@ module Sidekiq
             {
               '@status' => status[1],                                   # start or done
               '@run_time' => status[1] && result[1] && result[1].to_f,  # run time in seconds
-              '@message' => { 'msg' => message }
+              '@message' => message
             }
           end
         end
